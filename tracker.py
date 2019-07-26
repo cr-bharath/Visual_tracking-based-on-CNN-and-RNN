23380e4b86286345c31c4a1114450cb7062d1a55
cr-bharath <bharathcrenjith@gmail.com>
2019-07-26 16:09:23 -0400

    Chnages to tracker.py

--- tracker.py
+++ tracker.py
@@ -36,6 +36,8 @@ class re3Tracker():
 			prev_image, past_box = self.previous_frame
 
 		image_0, output_box0 = im_util.get_crop_input(prev_image, past_box, CROP_PAD, CROP_SIZE)
+		print('output_box0')
+		print(output_box0)
 		self.cropped_input[0, ...] = data_preparation(image_0)
 
 		image_1,_ = im_util.get_crop_input(image, past_box, CROP_PAD, CROP_SIZE)
@@ -47,6 +49,7 @@ class re3Tracker():
 			features = self.CNN(cropped_input_tensor.to(self.device))
 			predicted_bbox = self.RNN(features)
 		predicted_bbox_array = predicted_bbox.numpy()
+		print(predicted_bbox_array.squeeze())
         
         # Save initial LSTM states
 		if self.forward_count == 0:
@@ -61,7 +64,6 @@ class re3Tracker():
 			input_tensor = torch.from_numpy(np.float32(input_image))
 			input_tensor = input_tensor.view(-1,3,CROP_SIZE,CROP_SIZE)
 			self.RNN.reset()
-			print(input_tensor.size())
 			features = self.CNN(input_tensor)
 			prediction = self.RNN(features)
 		if starting_box is not None:
